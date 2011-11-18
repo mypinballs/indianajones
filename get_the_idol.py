14190c30b1c462958179c7bd3193bd87173a7b0e
Jim <jim@evilwillow.(none)>
2011-11-18 20:37:50 +0000

    added get the idol scene and improved mode select

---
+++ get_the_idol.py
@@ -0,0 +1,156 @@
+# Get The Idol Game Mode
+
+__author__="jim"
+__date__ ="$Jan 18, 2011 1:36:37 PM$"
+
+
+import procgame
+import locale
+from procgame import *
+
+base_path = config.value_for_key_path('base_path')
+game_path = base_path+"games/indyjones/"
+speech_path = game_path +"speech/"
+sound_path = game_path +"sound/"
+music_path = game_path +"music/"
+
+#locale.setlocale(locale.LC_ALL, 'en_GB')
+class ModeScoreLayer(dmd.TextLayer):
+	def __init__(self, x, y, font,mode, justify="left", opaque=False):
+		super(ModeScoreLayer, self).__init__(x, y, font,mode)
+		self.mode = mode
+                
+	def next_frame(self):
+		"""docstring for next_frame"""
+		# update score data from game mode
+		self.mode.update_score()
+
+		return super(ModeScoreLayer, self).next_frame()
+
+
+class Get_The_Idol(game.Mode):
+
+	def __init__(self, game, priority,mode_select):
+            super(Get_The_Idol, self).__init__(game, priority)
+
+            #setup link back to mode_select mode
+            self.mode_select = mode_select
+
+            #screen setup
+            timer = int(self.game.user_settings['Gameplay (Feature)']['Get The Idol Timer'])
+
+            self.score_layer = ModeScoreLayer(0, -1, self.game.fonts['07x5'], self)
+            self.timer_layer = dmd.TimerLayer(128, -1, self.game.fonts['07x5'],timer)
+            self.text_layer = dmd.TextLayer(128/2, 12, self.game.fonts['6x6_bold'], "center", opaque=False)
+
+
+            #sound setup
+            self.game.sound.register_music('gti_play', music_path+"get_the_idol.aiff")
+            self.game.sound.register_sound('target_hit', sound_path+"outlane.aiff")
+
+            #var setup
+            self.count = 0
+            self.hits = 3
+            self.score_value_boost = 5000000
+            self.score_value_start = 5000000
+            self.load_anim(0)
+            
+            self.reset()
+
+
+        def reset(self):
+           pass
+
+        def load_anim(self,count):
+            self.bgnd_anim = "dmd/get_the_idol_bgnd_"+str(count)+".dmd"
+            anim = dmd.Animation().load(game_path+self.bgnd_anim)
+            self.bgnd_layer = dmd.AnimatedLayer(frames=anim.frames,opaque=False,repeat=True,frame_time=2)
+
+
+        def mode_started(self):
+            #set display layer
+            self.layer = dmd.GroupedLayer(128, 32, [self.bgnd_layer,self.score_layer,self.timer_layer,self.text_layer])
+            self.game.sound.play_music('gti_play', loops=-1)
+
+            self.reset_drops()
+
+        def update_score(self):
+            score = self.game.current_player().score
+            self.score_layer.set_text(locale.format("%d", score, True))
+
+
+        def mode_tick(self):
+            pass
+
+        def mode_stopped(self):
+            #update mode select list - get the idol is mode 0 in list
+            current_list = self.game.get_player_stats('mode_select_list');
+            updated_list =current_list
+            updated_list[0]=1
+        
+            self.game.set_player_stats('mode_select_list',updated_list)
+
+            #stop music
+            self.game.sound.stop_music()
+            self.game.sound.play_music('general_play', loops=-1)
+            
+            #clear display
+            self.clear()
+
+        def mode_progression(self,type):
+
+            if self.count<self.hits:
+                
+                self.load_anim(self.count)
+
+
+                score_value = self.score_value_boost*self.count +self.score_value_start
+                self.game.set_player_stats('get_the_idol_score',score_value)
+                self.game.set_player_stats('last_mode_score',self.game.get_player_stats('get_the_idol_score' ))
+                #set text layers
+                self.text_layer.set_text(locale.format("%d",score_value,True),blink_frames=10,seconds=3)
+
+            
+                self.game.score(score_value)
+                self.game.sound.play('target_hit')
+            
+           
+                self.reset_drops()
+                self.count+=1
+            elif self.count==self.hits and type==1:
+                self.completed()
+
+        def completed(self):
+            self.bgnd_anim = "dmd/get_the_idol_completed.dmd"
+            anim = dmd.Animation().load(game_path+self.bgnd_anim)
+            self.layer = dmd.AnimatedLayer(frames=anim.frames,opaque=False,hold=True,frame_time=2)
+            #self.layer.transition = dmd.CrossFadeTransition(width=128,height=32)
+            #end scene when animation completes
+            self.layer.add_frame_listener(-1,self.mode_select.end_scene)
+            #self.delay(name='end_delay', event_type=None, delay=1, handler=self.mode_select.end_scene)
+
+            
+
+
+        def update_lamps(self):
+            print("Update Lamps")
+
+        def clear(self):
+            self.layer = None
+
+        def reset_drops(self):
+            self.game.coils.centerDropBank.pulse(100)
+
+
+        
+        def sw_dropTargetLeft_active(self, sw):
+            self.mode_progression(0)
+
+        def sw_dropTargetMiddle_active(self, sw):
+            self.mode_progression(0)
+
+        def sw_dropTargetRight_active(self, sw):
+            self.mode_progression(0)
+
+        def sw_centerEnter_active(self, sw):
+            self.mode_progression(1)
--- mode_select.py
+++ mode_select.py
@@ -8,21 +8,58 @@ __date__ ="$Jan 18, 2011 1:36:37 PM$"
 import procgame
 import locale
 from procgame import *
+from get_the_idol import *
 
+
+
+base_path = config.value_for_key_path('base_path')
+game_path = base_path+"games/indyjones/"
+speech_path = game_path +"speech/"
+sound_path = game_path +"sound/"
+music_path = game_path +"music/"
 
 class Mode_Select(game.Mode):
 
 	def __init__(self, game, priority):
             super(Mode_Select, self).__init__(game, priority)
-            self.text_layer = dmd.TextLayer(128/2, 7, self.game.fonts['07x5'], "center")
-            self.info_layer = dmd.TextLayer(128/2, 17, self.game.fonts['num_14x10'], "center")
-           
+            self.name_layer = dmd.TextLayer(128/2, 7, self.game.fonts['num_09Bx7'], "center")
+            self.info_layer = dmd.TextLayer(128/2, 18, self.game.fonts['07x5'], "center")
+
+            #setup sound
+            self.game.sound.register_sound('scene_started', sound_path+'mode_started.aiff')
+            self.game.sound.register_sound('scene_ended', sound_path+'mode_ended.aiff')
+
+            self.name_text =''
+            self.info_text =''
+
             self.lamp_list = ['getTheIdol','streetsOfCairo','wellOfSouls','ravenBar','monkeyBrains','stealTheStones','mineCart','ropeBridge','castleGrunwald','tankChase','theThreeChallenges','chooseWisely']
             self.select_list = [0,0,0,0,0,0,0,0,0,0,0,0]
             self.current_mode_num = self.game.get_player_stats('current_mode_num')
             self.choice_id =0
-            self.reset()
 
+            #set mode enabled flag from game settings
+            if self.game.user_settings['Gameplay (Feature)']['Mode Start Lit']!='Off':
+                self.mode_enabled = True
+            else:
+                self.mode_enabled = False
+
+            self.game.set_player_stats('mode_enabled',self.mode_enabled)
+
+            #setup game modes
+            self.get_the_idol = Get_The_Idol(self.game, 80,self)
+            self.streets_of_cairo = Get_The_Idol(self.game, 81,self)
+            self.well_of_souls = Get_The_Idol(self.game, 82,self)
+            self.raven_bar = Get_The_Idol(self.game, 83,self)
+            self.monkey_brains = Get_The_Idol(self.game, 84,self)
+            self.steal_the_stones = Get_The_Idol(self.game, 85,self)
+            self.mine_cart = Get_The_Idol(self.game, 86,self)
+            self.rope_bridge = Get_The_Idol(self.game, 87,self)
+            self.castle_grunwald = Get_The_Idol(self.game, 88,self)
+            self.tank_chase = Get_The_Idol(self.game, 89,self)
+            self.the_three_challenges = Get_The_Idol(self.game, 90,self)
+            self.choose_wisely = Get_The_Idol(self.game, 91,self)
+
+            self.reset()
 
         def reset(self):
             print("Main Mode Select Started")
@@ -33,6 +70,15 @@ class Mode_Select(game.Mode):
             for i in range(len(self.lamp_list)):
                 self.game.effects.drive_lamp(self.lamp_list[i],'off')
 
+            self.mode_start_lamp(self.mode_enabled)
+
+        def mode_start_lamp(self,flag):
+            lamp_name ='modeStart'
+            if flag:
+                self.game.effects.drive_lamp(lamp_name,'medium')
+            else:
+                self.game.effects.drive_lamp(lamp_name,'off')
+
 
         def mode_started(self):
             self.unplayed_scenes()
@@ -40,9 +86,47 @@ class Mode_Select(game.Mode):
         def mode_tick(self):
             pass
 
+        def mode_stopped(self):
+            if self.current_mode_num==0:
+                self.game.modes.remove(self.get_the_idol)
+            elif self.current_mode_num==1:
+                self.game.modes.remove(self.streets_of_cairo)
+            elif self.current_mode_num==2:
+                self.game.modes.remove(self.well_of_souls)
+            elif self.current_mode_num==3:
+                self.game.modes.remove(self.raven_bar)
+            elif self.current_mode_num==4:
+                self.game.modes.remove(self.monkey_brains)
+            elif self.current_mode_num==5:
+                self.game.modes.remove(self.steal_the_stones)
+            elif self.current_mode_num==6:
+                self.game.modes.remove(self.mine_cart)
+            elif self.current_mode_num==7:
+                self.game.modes.remove(self.rope_bridge)
+            elif self.current_mode_num==8:
+                self.game.modes.remove(self.castle_grunwald)
+            elif self.current_mode_num==9:
+                self.game.modes.remove(self.tank_chase)
+            elif self.current_mode_num==10:
+                self.game.modes.remove(self.the_three_challenges)
+            elif self.current_mode_num==11:
+                self.game.modes.remove(self.choose_wisely)
+
+            self.clear()
+
         def update_lamps(self):
-            print("Updating Selected Mode Lamp")
+            print("Updating Mode Lamps")
+
+            #current mode
             self.game.effects.drive_lamp(self.lamp_list[self.current_mode_num],'medium')
+
+            #completed modes
+            for i in range(len(self.lamp_list)):
+                if self.select_list[i]==1:
+                    self.game.effects.drive_lamp(self.lamp_list[i],'on')
+
+            #mode start
+            self.mode_start_lamp(self.mode_enabled)
 
 
         def unplayed_scenes(self,dirn=None):
@@ -73,8 +157,9 @@ class Mode_Select(game.Mode):
             #set new mode number
             self.current_mode_num = choice_list[self.choice_id]
 
-            #turn on relevent lamp
-            self.game.effects.drive_lamp(self.lamp_list[self.current_mode_num],'medium')
+            #update lamps
+            self.update_lamps()
+
             print("mode now active:"+str(self.lamp_list[self.current_mode_num]))
 
             #update player stats
@@ -105,9 +190,132 @@ class Mode_Select(game.Mode):
 
             self.move_left()
 
-
         def sw_rightRampMade_active(self, sw):
 
             self.move_right()
 
-       
+
+        def eject_ball(self):
+            self.game.coils.leftEject.pulse(15)
+     
+
+        def start_scene(self):
+            if self.mode_enabled:
+
+                #default value
+                timer=30
+
+                #play sound
+                self.game.sound.play("scene_started")
+
+                if self.current_mode_num==0:
+                    self.game.modes.add(self.get_the_idol)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Get The Idol Timer']
+                    self.name_text = 'GET THE IDOL'
+                    self.info_text = 'HIT CENTER DROP TARGETS'
+
+                elif self.current_mode_num==1:
+                    self.game.modes.add(self.streets_of_cairo)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Streets Of Cairo Timer']
+                    self.name_text = 'STREETS OF CAIRO'
+                    self.info_text = 'SHOOT RAMPS TO FIND MARION'
+
+                elif self.current_mode_num==2:
+                    self.game.modes.add(self.well_of_souls)
+                    #timer = self.game.user_settings['Gameplay (Feature)']['Well Of Souls Timer']
+                    self.name_text = 'WELL OF SOULS'
+                    self.info_text = 'SHOOT CENTER HOLE'
+
+                elif self.current_mode_num==3:
+                    self.game.modes.add(self.raven_bar)
+                    #timer = self.game.user_settings['Gameplay (Feature)']['Raven Bar Timer']
+                    self.name_text = 'RAVEN BAR'
+                    self.info_text = 'XXX'
+
+                elif self.current_mode_num==4:
+                    self.game.modes.add(self.monkey_brains)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Monkey Brains Timer']
+                    self.name_text = 'MONKEY BRAINS'
+                    self.info_text = 'SHOOT LIT SHOTS'
+
+                elif self.current_mode_num==5:
+                    self.game.modes.add(self.steal_the_stones)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Steal The Stones Timer']
+                    self.name_text = 'STEAL THE STONES'
+                    self.info_text = 'XXX'
+
+                elif self.current_mode_num==6:
+                    self.game.modes.add(self.mine_cart)
+                    #timer = self.game.user_settings['Gameplay (Feature)']['Mine Cart Timer']
+                    self.name_text = 'MINE CART'
+                    self.info_text = 'XXX'
+
+                elif self.current_mode_num==7:
+                    self.game.modes.add(self.rope_bridge)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Rope Bridge Timer']
+                    self.name_text = 'ROPE BRIDGE'
+                    self.info_text = 'XXX'
+
+                elif self.current_mode_num==8:
+                    self.game.modes.add(self.castle_grunwald)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Castle Grunwald Timer']
+                    self.name_text = 'CASTLE GRUNWALD'
+                    self.info_text = 'SHOOT CAPTIVE BALL'
+
+                elif self.current_mode_num==9:
+                    self.game.modes.add(self.tank_chase)
+                    timer = self.game.user_settings['Gameplay (Feature)']['Tank Chase Timer']
+                    self.name_text = 'TANK CHASE'
+                    self.info_text = 'SHOOT LIT LOOPS'
+
+                elif self.current_mode_num==10:
+                    self.game.modes.add(self.the_three_challenges)
+                    timer = self.game.user_settings['Gameplay (Feature)']['The 3 Challenges Timer']
+                    self.name_text = 'THE 3 CHALLENGES'
+                    self.info_text = 'HIT CENTER DROP TARGETS'
+
+                elif self.current_mode_num==11:
+                    self.game.modes.add(self.choose_wisely)
+                    #timer = self.game.user_settings['Gameplay (Feature)']['Choose Wisely Timer']
+                    self.name_text = 'CHOOSE WISELY'
+                    self.info_text = 'SHOOT LIT SHOT, WATCH CAREFULLY!'
+
+
+                anim = dmd.Animation().load(game_path+"dmd/start_scene.dmd")
+                self.animation_layer = dmd.AnimatedLayer(frames=anim.frames,hold=True,frame_time=2)
+                self.animation_layer.add_frame_listener(-1,self.mode_text)
+
+                self.layer = dmd.GroupedLayer(128, 32, [self.animation_layer,self.name_layer,self.info_layer])
+
+                self.delay(name='scene_timeout', event_type=None, delay=timer, handler=self.end_scene)
+                self.delay(name='eject_delay', event_type=None, delay=2, handler=self.eject_ball)
+                self.delay(name='clear_delay', event_type=None, delay=2, handler=self.clear)
+
+                self.mode_enabled=False
+                
+            else:
+                self.delay(name='eject_delay', event_type=None, delay=0.5, handler=self.eject_ball)
+
+        def mode_text(self):
+            self.name_layer.set_text(self.name_text)
+            self.info_layer.set_text(self.info_text)
+
+
+        def end_scene(self):
+            #play sound
+            self.game.sound.play("scene_ended")
+
+            #display mode total on screen
+            bgnd_layer = dmd.FrameLayer(opaque=False, frame=dmd.Animation().load(game_path+"dmd/scene_ended_bgnd.dmd").frames[0])
+            self.info_layer.set_text(str(self.game.get_player_stats('last_mode_score')))
+            self.layer = dmd.GroupedLayer(128, 32, [bgnd_layer,self.name_layer,self.info_layer])
+
+            #run mode ended to remove active game scene
+            self.delay(name='scene_timeout', event_type=None, delay=2, handler=self.mode_stopped)
+            
+
+        def clear(self):
+            self.layer=None
+
+        def sw_leftEject_active_for_500ms(self,sw):
+            self.start_scene()
